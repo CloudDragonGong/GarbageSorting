61ea40b421dca326dd1208c3bb36af1f9ed136ba
CloudDragonGong <dragongong20040318@gmail.com>
2024-06-08 04:12:39 +0800

    feat

--- model/YOLO_classifier.py
+++ model/YOLO_classifier.py
@@ -5,9 +5,10 @@ class YoloClassifier():
     def __init__(self):
         self.client = None # 识别器
 
-    def classfiyOneImg(self,single_img_file_name:str, out_img_folder:str)->str:
+    def classfiyOneImg(self,input_path:str,output_path:str)->str:
         """
         根据single_img_file_name,img_folder路径的图片识别
         :return:save后的filename,save的file也放在 img_folder 中
         """
+
         pass
---
+++ utils/img.py
@@ -0,0 +1,20 @@
+import os
+import random
+import shutil
+
+def mutation(output_dir,output_filename):
+    img = get_random_image_filename(output_dir)
+    copy_file_to_directory(os.path.join(output_dir,img), "./imgs",output_filename)
+
+def copy_file_to_directory(src, dst,output_filename):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    shutil.copyfile(src,os.path.join(dst,os.path.basename(output_filename)))
+
+def get_random_image_filename(folder_path):
+    files = os.listdir(folder_path)
+    image_files = [f for f in files if f.lower().endswith(('jpg', 'jpeg', 'png'))]
+    if not image_files:
+        return None
+    random_image = random.choice(image_files)
+    return random_image
